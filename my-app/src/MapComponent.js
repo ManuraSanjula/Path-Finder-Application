69751bd1311d7eb9dcf9ddb771d2490acede5ecd
Manura Sanjula <w.m.manurasanjula2003@gmail.com>
2024-08-04 22:31:47 +0530

    mapbox error fixed

--- my-app/src/MapComponent.js
+++ my-app/src/MapComponent.js
@@ -8,10 +8,12 @@ const MapComponent = () => {
   const { location: userLocation, des } = location.state;
 
   useEffect(() => {
-    const mapboxAccessToken = 'pk.eyJ1IjoibWFudXgyMDAzIiwiYSI6ImNseXpjcTQ3ZjB2dXMyaW9yODhhczl6bzEifQ.TePta50hse5LBObP7LxSdg';
+    const mapboxAccessToken = 'pk.eyJ1IjoibWFudXgyMDAzIiwiYSI6ImNsemZzNm85ODEwazYyc3NhMnN0eTExNHQifQ.jsbN6UwtLdzriyJU2-7JxA';
     const L = window.L;
 
     if (!mapRef.current) {
+      console.log("Initializing the map...");
+
       mapRef.current = L.map('map').setView([userLocation.latitude, userLocation.longitude], 13);
 
       L.tileLayer(`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=${mapboxAccessToken}`, {
@@ -27,20 +29,29 @@ const MapComponent = () => {
 
       async function getDirections(start, end) {
         const url = `https://api.mapbox.com/directions/v5/mapbox/driving/${start[1]},${start[0]};${end[1]},${end[0]}?geometries=geojson&access_token=${mapboxAccessToken}`;
+        console.log("Fetching directions from: ", url);
 
         const response = await fetch(url);
         const data = await response.json();
-        const route = data.routes[0];
+        console.log("Directions data: ", data);
 
-        return route;
+        if (data.routes && data.routes.length > 0) {
+          const route = data.routes[0];
+          return route;
+        } else {
+          console.error("No routes found");
+          return null;
+        }
       }
 
       async function displayDirections() {
         const route = await getDirections(start, end);
+        if (!route) return;
+
         const coords = route.geometry.coordinates;
         const latlngs = coords.map(coord => [coord[1], coord[0]]);
 
-        const polyline = L.polyline(latlngs, { color: 'blue' }).addTo(mapRef.current);
+        const polyline = L.polyline(latlngs, { color: 'blue', weight: 5, opacity: 0.7, smoothFactor: 1 }).addTo(mapRef.current);
         mapRef.current.fitBounds(polyline.getBounds());
 
         const instructions = route.legs[0].steps;
@@ -54,7 +65,7 @@ const MapComponent = () => {
   }, [userLocation, des]);
 
   return (
-    <div id="map"></div>
+    <div id="map" style={{ height: '1000px', width: '100%' }}></div>
   );
 };
 
